a6cc6b972ee26d999c210fb6b45eed9ca6accd67
MichaelThangam <124876908+MichaelThangam@users.noreply.github.com>
2024-10-27 20:41:10 +0530

    Product changes

--- src/pages/commonCompontes/Pagination.js
+++ src/pages/commonCompontes/Pagination.js
@@ -4,11 +4,15 @@ const Pagination = ({currentPage,perPage,totalPage,setCurrentPage}) => {
     for(let i = 1; i < Math.ceil(totalPage / perPage); i++){
         pages.push(i);
     }
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+        window.scrollTo({ top: 0, behavior: 'smooth' }); 
+    };
 
     return(
         <div className="paginationBtnGroup">
             {pages.map((page,index) => (
-                <button key={index} className={currentPage === index + 1 ? 'pageBtn active': 'pageBtn'} onClick={() => setCurrentPage(page)}>{page}</button>
+                <button key={index} className={currentPage === index + 1 ? 'pageBtn active': 'pageBtn'} onClick={() => handlePageChange(page)}>{page}</button>
             ))}
         </div>
     )
